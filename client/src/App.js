040a055df595902e78a4f29ac922cab91887e7a5
MohitSaini <mailtomohit2002@gmail.com>
2022-10-10 18:15:26 +0530

    feat(frontend) : added preloader

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React  from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Home from './pages/Home'
@@ -9,24 +9,40 @@ import SellerRegister from './pages/Authentication/SellerRegister'
 import BusinessPage from './pages/BusinessPage'
 import RatingForm from './components/RatingForm'
 import SaleForm from './components/SaleForm'
+import preloader from './assets/newstar.gif'
 
 const App = () => {
+	useEffect(() => {
+		var preloader = document.getElementById('preLoader');
+		window.addEventListener("load", () => {
+			preloader.style.display = 'none'
+		})
+	}, [])
+
 	return (
-		<div className='App'>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/dashboard" element={<Dashboard />} />
-					<Route path="/sellers/:id" element={<BusinessPage />} />
-					<Route path="/sellerLogin" element={<SellerLogin />} />
-					<Route path="/sellerRegister" element={<SellerRegister />} />
-					<Route path="/ratingForm" element={<RatingForm />} />
-					<Route path="/saleForm" element={<SaleForm />} />
-				</Routes>
-			</BrowserRouter>
-		</div>
+		<>
+			{/* code for preloader */}
+			<div id="preLoader" className={`bg-white h-[100vh] w-full fixed z-40 bg-no-repeat bg-center flex justify-center items-center`}>
+				<img src={preloader} className='w-[22rem] h-[16rem]' />
+			</div>
+
+
+			<div className='App'>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+						<Route path="/dashboard" element={<Dashboard />} />
+						<Route path="/sellers/:id" element={<BusinessPage />} />
+						<Route path="/sellerLogin" element={<SellerLogin />} />
+						<Route path="/sellerRegister" element={<SellerRegister />} />
+						<Route path="/ratingForm" element={<RatingForm />} />
+						<Route path="/saleForm" element={<SaleForm />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</>
 	)
 }
 
