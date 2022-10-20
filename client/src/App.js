8754819db6cbf4494c7b341566b4bad88d98aecc
Mohit Saini <mailtomohit2002@gmail.com>
2022-10-20 11:40:58 +0530

    feat(frontend): Preloader Added (#24)

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React  from 'react'
+import React,{useEffect} from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Home from './pages/Home'
@@ -9,10 +9,23 @@ import SellerRegister from './pages/Authentication/SellerRegister'
 import BusinessPage from './pages/BusinessPage'
 import RatingForm from './components/Forms/RatingForm'
 import SaleForm from './components/Forms/SaleForm'
+import preloader from './assets/newstar.gif'
 
 const App = () => {
+	useEffect(() => {
+		var preloader = document.getElementById('preLoader');
+		window.addEventListener("load", () => {
+			preloader.style.display = 'none'
+		})
+	}, [])
+
 	return (
 		<div className='App'>
+			{/* code for preloader */}
+			<div id="preLoader" className={`bg-white h-[100vh] w-full fixed z-40 bg-no-repeat bg-center flex justify-center items-center`}>
+				<img src={preloader} className='w-[22rem] h-[16rem]' />
+			</div>
+
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
