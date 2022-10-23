737b2fba33d2c6a632ebd26536f89fc37f5f4998
warriorBunny013 <mona23sonai@gmail.com>
2022-10-23 09:41:32 +0530

    improvement(frontend): made navbar mobile responsive

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,103 +1,33 @@
-// import React from 'react'
-// import Button from '@mui/material/Button';
-// import ButtonGroup from '@mui/material/ButtonGroup';
-// import Avatar from '@mui/material/Avatar';
-// import { deepPurple } from '@mui/material/colors';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 
-// const Navbar = () => {
-//   return (
-//     <>
-//       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-//         <a className="navbar-brand p-2.5 ml-4" href="/">IMdbBusiness</a>
-//         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <div className="collapse navbar-collapse p-2.5" id="navbarSupportedContent">
-//           <ul className="navbar-nav mr-auto">
-//             <li className="nav-item active">
-//               <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-//             </li>
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import logoImageSrc from "../../assets/logo.jpg";
 
-//             <li className="nav-item dropdown">
-//               <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-//                 Login
-//               </a>
-//               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                 <a className="dropdown-item" href="/login">Login as a Customer</a>
-//                 <a className="dropdown-item" href="/sellerLogin">Login as a Seller</a>
-//               </div>
-//             </li>
-//             <li className="nav-item dropdown">
-//               <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-//                 Register
-//               </a>
-//               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                 <a className="dropdown-item" href="/register">Register as a Customer</a>
-//                 <a className="dropdown-item" href="/sellerRegister">Register as a Seller</a>
-//               </div>
-//             </li>
-//             <li className="nav-item dropdown">
-
-//               <Avatar  sx={{ width: 36, height: 34 , bgcolor: deepPurple[500] }}>{ }</Avatar>
-//                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                   <a className="dropdown-item" href="/login">My feedbacks</a>
-//                   <a className="dropdown-item" href="/sellerLogin">My Profile</a>
-//                   <a className="dropdown-item" href="/sellerLogin">Account Settings</a>
-//                   <a className="dropdown-item" href="/sellerLogin">Logout</a>
-//                 </div>
-
-//             </li>
-//           </ul>
-//         </div>
-//         <div className="flex justify-end px-10">
-//           <ButtonGroup variant="outlined" aria-label="outlined primary button group">
-//             <Button>Favourite Brands</Button>
-//             <Button>Most Shopped Brands</Button>
-//             <Button>Your Reminders</Button>
-//           </ButtonGroup>
-//           <div className="nav-item ml-5 dropdown">
-//             <Avatar sx={{ bgcolor: deepPurple[500] }}>{ }</Avatar>
-//             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-//               <a className="dropdown-item" href="/login">My feedbacks</a>
-//               <a className="dropdown-item" href="/sellerLogin">My Profile</a>
-//               <a className="dropdown-item" href="/sellerLogin">Account Settings</a>
-//               <a className="dropdown-item" href="/sellerLogin">Logout</a>
-//             </div>
-//           </div>
-//         </div>
-//       </nav>
-//     </>
-//   )
-// }
-
-// export default Navbar
-
-
-
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import logoImageSrc from '../../assets/logo.jpg'
-
-const pages = ['Login', 'Register'];
-const settings = ['My Profile', 'Account Settings', 'My favourites', 'Reminders', 'Logout'];
+const pages = ["Sale", "About Us", "Contact", "Login"];
+const settings = [
+  "My Profile",
+  "Account Settings",
+  "My favourites",
+  "Reminders",
+  "Logout",
+];
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      main: '#1976d2',
+      main: "#1976d2",
     },
   },
 });
@@ -123,111 +53,110 @@ const Navbar = () => {
 
   return (
     <ThemeProvider theme={darkTheme}>
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <img alt="logo" className="header-logo" src={logoImageSrc} />
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            {/* LOGO DESKTOP*/}
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <img alt="logo" className="header-logo" src={logoImageSrc} />
+            </Box>
+
+            {/* MENU ICON FOR MOBILE */}
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            {/* LOGO MOBILE */}
+            <Box
+              sx={{ mr: 2, flexGrow: 1, display: { xs: "flex", md: "none" } }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+              <img alt="logo" className="header-logo" src={logoImageSrc} />
+            </Box>
+            {/* MENU ITEMS:SALE,ABOUT US, CONTACT,LOGIN DESKTOP */}
+            <Box
               sx={{
-                display: { xs: 'block', md: 'none' },
+                flexGrow: 1,
+                justifyContent: "flex-end",
+                mr: 2,
+                display: { xs: "none", md: "flex" },
               }}
             >
               {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
               ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+            </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp"/>
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </ThemeProvider>
   );
 };
 export default Navbar;
-
