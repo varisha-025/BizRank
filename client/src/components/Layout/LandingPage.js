c2029fdf4d8303da3295ad2b3ef454be4e962d54
srt.lna <96660357+shrutilenka@users.noreply.github.com>
2022-10-28 21:38:37 +0530

    fix [frontend]:LandingPage.js (1st) added (#44)
    * 1st part of landing page
    
    * updated images with given illustrations
    
    * updated requested changes
    
    * 4th
    
    Co-authored-by: shrutilenka <shrutilenka1202@gmail.com>

---
+++ client/src/components/Layout/LandingPage.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import {BsCircleFill} from 'react-icons/bs';
+import img1 from "../../assets/body-img1.png";
+import img2 from "../../assets/body-img2.png";
+import img3 from "../../assets/body-img3.png";
+
+const LandingPage=()=> {
+    return(
+        <div className='bg-black text-white'>
+            <div className='w-[44rem] mx-auto text-center'>
+            <h3 className='text-[2.5rem] font-bold'>Stand out with a free Business Profile</h3>
+            <p className='text-[1.25rem] pt-3 font-thin'>Turn people who find you on this website into new customers with a free Business Profile for your 
+                storefront or service area.Personalise your profile with promotion product photos, offers, posts, and more.</p>
+            </div>
+            <div  className='w-[46rem] mx-auto mt-[4rem] text-center'>
+                <h3 className='text-[2.5rem] font-bold'>First impression is the last impresion</h3>
+                <p  className='text-[1.25rem] pt-3 font-thin'>Highlight essential info and show what makes your business unique, right on your Business Profile.</p>
+            </div>
+            <div className='flex flex-row space-x-[4rem] justify-center py-[15rem] px-[5rem] '>
+                <div className='flex-row space-x-2 inline-flex w-[30rem] h-[34rem] '>
+                    <div className="text-[3.5rem] text-[#dabb59] pr-1">
+                        <div className='relative items-center flex'>
+                         <BsCircleFill  />
+                         <p className='text-white absolute text-[2.5rem] ml-4'>1</p>
+                        </div>
+                    </div>
+                    <div class="flex justify-center">
+                      <div class="rounded-xl shadow-lg bg-[#3d3d3d] max-w-sm ">
+                        <div class="pt-3 px-4">
+                          <h5 class="text-white text-[1.25rem] font-medium  pt-2">Add essential information</h5>
+                        </div>
+                          <img class="rounded-t-lg px-6 pt-8" src={img1} alt="an illustrtaion for information"/>
+                        <div class="px-4 pt-[1rem]">
+                          <p class="text-white text-[1.075rem]">
+                            Let customers know info like phone number, products you sell, health and safety
+                            measures so customers know what to expect.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+                <div className='flex-row space-x-2 inline-flex w-[30rem] h-[34rem] '>
+                    <div className="text-[3.5rem] text-[#dabb59]">
+                        <div className='relative items-center flex'>
+                         <BsCircleFill  />
+                         <p className='text-white absolute text-[2.5rem] ml-4'>2</p>
+                        </div>
+                    </div>
+                    <div class="flex justify-center">
+                      <div class="rounded-xl shadow-lg bg-[#3d3d3d] max-w-sm ">
+                        <div class="pt-2 px-4">
+                          <h5 class="text-white text-[1.25rem] font-medium mb-2 pt-2">Share product photos, brand logo and your goals</h5>
+                        </div>
+                          <img class="rounded-t-lg px-6 pt-1" src={img2} alt="an illustration for branding and awareness"/>
+                        
+                        <div class="px-4 pt-[1rem]">
+                          <p class="text-white text-[1.075rem] text-base ">
+                            Showcase your brand logo, product photos and what are your goals for their business to stand out more.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+
+                <div className='flex-row space-x-2 inline-flex w-[30rem] h-[34rem] '>
+                    <div className="text-[3.5rem] text-[#dabb59]">
+                        <div className='relative items-center flex'>
+                         <BsCircleFill  />
+                         <p className='text-white absolute text-[2.5rem] ml-4'>3</p>
+                        </div>
+                    </div>
+                    <div class="flex justify-center">
+                      <div class="rounded-xl shadow-lg bg-[#3d3d3d] max-w-sm ">
+                        <div class="px-4 pt-2">
+                          <h5 class="text-white text-[1.25rem] font-medium  pt-2">Add sale info</h5>
+                        </div>
+                          <img class="rounded-t-lg px-6 pt-8" src={img3} alt="an illustration for sale"/>
+                        <div class="px-4 pt-[1rem]">
+                          <p class="text-white text-[1.075rem] text-base">
+                            Add info about your sale so customers can set a reminder and miss it!
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage
